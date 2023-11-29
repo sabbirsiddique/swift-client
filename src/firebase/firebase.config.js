70157af8973a9c4df4a92258ebf2ab1f7e046d74
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-11-30 03:44:44 +0600

    firebase setup complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhNWsJjZpCOIspYwcwFUxP8GpQxeH_i1A",
+  authDomain: "swift-send-7ab7b.firebaseapp.com",
+  projectId: "swift-send-7ab7b",
+  storageBucket: "swift-send-7ab7b.appspot.com",
+  messagingSenderId: "809024677188",
+  appId: "1:809024677188:web:ba1cbb84677702fd1ae311"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
